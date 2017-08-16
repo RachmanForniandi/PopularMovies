eae1267862f0a33e79c1564eb114434eae65d548
Alexey Sobolevsky <sobolevsky.alexey@gmail.com>
2017-08-16 18:26:49 +0300

    Configure trailers list

--- app/src/main/java/com/poissondumars/popularmovies/MovieTrailersFragment.java
+++ app/src/main/java/com/poissondumars/popularmovies/MovieTrailersFragment.java
@@ -1,23 +1,37 @@
 package com.poissondumars.popularmovies;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import com.poissondumars.popularmovies.api.MoviesDbJSONHelper;
+import com.poissondumars.popularmovies.api.TheMoviesDbApiClient;
 import com.poissondumars.popularmovies.data.Movie;
+import com.poissondumars.popularmovies.data.MovieTrailersAdapter;
 import com.poissondumars.popularmovies.data.Trailer;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MovieTrailersFragment extends Fragment {
 
-    private Trailer[] mTrailers;
+    private MovieTrailersAdapter mTrailersAdapter;
     private Movie mMovie;
 
-    public MovieTrailersFragment() {}
+    @BindView(R.id.rv_trailers_list)
+    RecyclerView mTrailersRecycleView;
+
+    @BindView(R.id.pb_trailers_loading_indicator)
+    ProgressBar mLoadingIndicator;
+
+    public MovieTrailersFragment() { }
 
     public static MovieTrailersFragment instanseWith(Movie movie) {
         MovieTrailersFragment instance = new MovieTrailersFragment();
@@ -31,6 +45,53 @@ public class MovieTrailersFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_movie_trailers, container, false);
         ButterKnife.bind(this, view);
 
+        mTrailersAdapter = new MovieTrailersAdapter();
+        mTrailersRecycleView.setAdapter(mTrailersAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext());
+        mTrailersRecycleView.setLayoutManager(layoutManager);
+        mTrailersRecycleView.setHasFixedSize(true);
+
+        loadTrailers();
+
         return view;
+    }
+
+    private void loadTrailers() {
+        new FetchTrailersTask().execute(mMovie.id);
+    }
+
+    private class FetchTrailersTask extends AsyncTask<Integer, Void, Trailer[]> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Trailer[] doInBackground(Integer... params) {
+            if (params.length == 0) {
+                return null;
+            }
+
+            Integer movieId = params[0];
+            String response = TheMoviesDbApiClient.getTrailersForMovie(movieId);
+            if (response != null) {
+                return MoviesDbJSONHelper.parseTrailersFromJson(response);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Trailer[] trailers) {
+            super.onPostExecute(trailers);
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+
+            if (trailers != null) {
+                mTrailersAdapter.setTrailers(trailers);
+            }
+        }
     }
 }
--- app/src/main/java/com/poissondumars/popularmovies/data/MovieTrailersAdapter.java
+++ app/src/main/java/com/poissondumars/popularmovies/data/MovieTrailersAdapter.java
@@ -1,15 +1,23 @@
 package com.poissondumars.popularmovies.data;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.poissondumars.popularmovies.R;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MovieTrailersAdapter {
+public class MovieTrailersAdapter extends RecyclerView.Adapter<MovieTrailersAdapter.MovieTrailersAdapterViewHolder> {
 
     class MovieTrailersAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-
+        @BindView(R.id.tv_trailer_name)
+        TextView trailerNameTextView;
 
         MovieTrailersAdapterViewHolder(View view) {
             super(view);
@@ -23,4 +31,34 @@ public class MovieTrailersAdapter {
         }
     }
 
+    private Trailer[] mTrailers;
+
+    @Override
+    public MovieTrailersAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.movie_trailer_item, parent, false);
+
+        return new MovieTrailersAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieTrailersAdapterViewHolder holder, int position) {
+        Trailer trailer = mTrailers[position];
+
+        if (trailer == null) return;
+
+        holder.trailerNameTextView.setText(trailer.name);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mTrailers == null) return 0;
+        return mTrailers.length;
+    }
+
+    public void setTrailers(Trailer[] trailers) {
+        mTrailers = trailers;
+        notifyDataSetChanged();
+    }
 }
